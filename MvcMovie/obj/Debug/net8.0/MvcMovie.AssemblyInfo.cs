ece3ec63f084248e8cb5c968a15388f6ae7ee5fa
Masha-bat <batyushkova03@mail.ru>
2024-09-18 11:21:58 +0300

    изменение начальной страницы

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc9fdbf0326d8659ad93b7ed42beba693c4cfd04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d167cce7d5346087775435b4dd88f334c4e56f24")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
